e39d67b421690c53886edf4e1e24f2aced59ac33
Johan Silkens <johan.silkens@elmos.be>
2024-10-26 16:22:20 +0200

    (1) Setup project structure

---
+++ application/src/test/java/be/jsilkens/devbooks/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jsilkens.devbooks.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
