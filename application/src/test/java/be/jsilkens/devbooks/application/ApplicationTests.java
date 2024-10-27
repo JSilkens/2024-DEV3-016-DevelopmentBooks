c2e1e843e5039bcb1dfbf00b5ec8855636d50238
Johan Silkens <johan.silkens@elmos.be>
2024-10-27 17:50:01 +0100

    (1) Add component scan

--- application/src/test/java/be/jsilkens/devbooks/application/ApplicationTests.java
+++ application/src/test/java/be/jsilkens/devbooks/application/ApplicationTests.java
@@ -2,8 +2,10 @@ package be.jsilkens.devbooks.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"local"})
 class ApplicationTests {
 
 	@Test
